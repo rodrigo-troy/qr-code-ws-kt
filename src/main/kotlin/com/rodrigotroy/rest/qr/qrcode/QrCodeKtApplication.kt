0cdce4ccb632911963a6a0af8d032b5bbc403796
Rodrigo Troy <1328268+rodrigo-troy@users.noreply.github.com>
2025-06-01 16:19:44 -0400

    Initialize Spring Boot Kotlin project with default configurations and setup.

---
+++ src/main/kotlin/com/rodrigotroy/rest/qr/qrcode/QrCodeKtApplication.kt
@@ -0,0 +1,11 @@
+package com.rodrigotroy.rest.qr.qrcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QrCodeKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<QrCodeKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/rodrigotroy/rest/qr/qrcode/QrCodeKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rodrigotroy.rest.qr.qrcode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QrCodeKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
