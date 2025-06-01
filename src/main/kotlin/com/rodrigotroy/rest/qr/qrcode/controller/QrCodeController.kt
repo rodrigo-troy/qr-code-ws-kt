63eb35f8ec74a8577d25bed69091958c58321232
Rodrigo Troy <1328268+rodrigo-troy@users.noreply.github.com>
2025-06-01 17:14:07 -0400

    Update `qrcode` endpoint in `QrCodeController` to generate a blank 250x250 PNG image response

--- src/main/kotlin/com/rodrigotroy/rest/qr/qrcode/controller/QrCodeController.kt
+++ src/main/kotlin/com/rodrigotroy/rest/qr/qrcode/controller/QrCodeController.kt
@@ -1,19 +1,34 @@
 package com.rodrigotroy.rest.qr.qrcode.controller
 
-import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.awt.Color
+import java.awt.image.BufferedImage
 
 @RestController
 @RequestMapping("/api/qrcode")
 class QrCodeController {
 
     @GetMapping
-    fun qrcode(): ResponseEntity<Void> {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build()
+    fun qrcode(): ResponseEntity<BufferedImage> {
+        val width = 250
+        val height = 250
+        
+        val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+        val graphics = image.createGraphics()
+        
+        graphics.color = Color.WHITE
+        graphics.fillRect(0, 0, width, height)
+        graphics.dispose()
+        
+        return ResponseEntity
+            .ok()
+            .contentType(MediaType.IMAGE_PNG)
+            .body(image)
     }
 
     @GetMapping("/generate")
